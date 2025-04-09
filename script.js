a7c4374fd15bef538c7a2ff6e564c8c84d8dd903
GabrielVanco <gabrielvanco111223@gmail.com>
2025-04-08 21:42:32 -0300

    Inserindo condições das cores

--- script.js
+++ script.js
@@ -29,4 +29,21 @@ function colorirDia(){
 
     // Declara e inicializa o contador
     let contadorAzul = 0, contadorVerde = 0, contadorRosa = 0, contadorRoxo = 0;
+
+    for (let i = 0; i < elementos.length; i++) {
+        var estilo = window.getComputedStyle(elementos[i]);
+
+        // Obtém a cor de fundo atual da célula
+        var corEstilo = estilo.backgroundColor;
+        
+        if(corEstilo === "rgb(0, 0, 255)" || corEstilo === "lightblue"){
+            contadorAzul++;
+        } else if(corEstilo === "rgb(0, 128, 0)" || corEstilo === "lightgreen"){
+            contadorVerde++;
+        } else if(corEstilo === "rgb(255, 0, 255)" || corEstilo === "lightpink"){
+            contadorRosa++;
+        } else if(corEstilo === "rgb(128, 0, 128)" || corEstilo === "plum"){            
+            contadorRoxo++;
+        }
+    }
 }
